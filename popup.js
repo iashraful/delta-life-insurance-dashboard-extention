7bf69c643dc4c88114857aea45f676ecb64af34a
Mohammad Ashraful <a5hraful@outlook.com>
2024-10-03 00:24:23 +0600

    Working on the network data

--- popup.js
+++ popup.js
@@ -1,1 +1,14 @@
 console.log('This is a popup!');
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      // Filter requests based on your criteria
+      if (details.url.startsWith("https://hiclaim.deltalife.org/api/")) {
+        // Send the request to the background script
+        console.log(details)
+        chrome.runtime.sendMessage({ request: details });
+      }
+    },
+    { urls: ["https://hiclaim.deltalife.org/*"] },
+    ["requestBody"]
+  );
